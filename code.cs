0ab7467d9f92258838dfa0ed4050e7488eb080bf
oujialin <oujialin@dossen.com>
2019-11-20 16:05:06 +0800

    333

--- code.cs
+++ code.cs
@@ -5,6 +5,24 @@ public class OTASecurity
             var token = 2;
             return new { token = token, expires = int.MaxValue }.ObjectToJson();
         }
+        public static string Add(string AppId, string AppKey)
+        {
+            var token = 2;
+            return new { token = token, expires = int.MaxValue }.ObjectToJson();
+        }
+		
+		public class OTASecurity
+    {
+        public static string GenerateTokenString(string AppId, string AppKey)
+        {
+            var token = 2;
+            return new { token = token, expires = int.MaxValue }.ObjectToJson();
+        }
+        public static string Add(string AppId, string AppKey)
+        {
+            var token = 2;
+            return new { token = token, expires = int.MaxValue }.ObjectToJson();
+        }
+    }
 
-        
     }
